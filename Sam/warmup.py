934b0a5a28b31304b9daf016edb7f53edc3390d7
Samuel Kim <samuelkim@college.harvard.edu>
2014-02-05 18:46:00 -0500

    K-means warmup working, bugs remaining. Tested on sample data.

--- Sam/warmup.py
+++ Sam/warmup.py
@@ -1,4 +1,8 @@
 import numpy as np
+import random
+
+#TODO: clean code, separate into different files
+#fix div/0 error
 
 def unpickle_CIFAR(file):
     """Import CIFAR datasets"""
@@ -12,7 +16,7 @@ def unpickle_CIFAR(file):
     return dict
 
 def init_kmeans(data, k):
-    return [random(0,k-1) for x in range(len(data))]
+    return [random.randint(0,k-1) for x in range(len(data))]
 
 def add_lists(a,b):
     return [x + y for x, y in zip(a, b)]
@@ -20,39 +24,51 @@ def add_lists(a,b):
 def div_list_by_num(list,a):
     return [x/a for x in list]
 
-def cluster_mean(data, ks):
-    k=len(ks)
+def cluster_mean(data, k, ks):
     #num data points for each cluster
     cluster_nums = [0]*k
     #total sum of vectors for each cluster
     cluster_sums = [[0]*len(data)]*k
     for x in range(len(ks)):
         cluster_nums[ks[x]] += 1
-        cluster_sums[ks[x]] = add_lists[cluster_sums[ks[x]], data[x]]
+        cluster_sums[ks[x]] = add_lists(cluster_sums[ks[x]], data[x])
     return [div_list_by_num(cluster_sums[i],cluster_nums[i]) for i in range(k)]
 
 def distances(data, us):
-    diff = data[np.newaxis,:,:] - us[:,np.newaxis,:]
-    dist = np.sum(diff**2,axis=-1)
-    return dist
+    return [[list_dist(x, y) for y in us] for x in data]
+
+def list_dist(a, b):
+    diff = [x - y for x, y in zip(a, b)]
+    return sum([x**2 for x in diff])
 
 def update_cluster(data, ks):
-    dist = distances(data, cluster_mean(data, ks))
+    dist = distances(data, cluster_mean(data, k, ks))
     for i in range(len(ks)):
-        val, idx = min((val, idx) for (idx, val) in enumerate(dist(i)))
+        val, idx = min((val, idx) for (idx, val) in enumerate(dist[i]))
         ks[i] = idx
     return ks
 
-raw_data = unpickle_CIFAR('data_batch_1')
-data = raw_data['data']
-k=10
+#raw_data = unpickle_CIFAR('data_batch_1')
+#data = raw_data['data']
+
+data = np.array([[10, 0, 0, 0],
+              [11, 0, 0, 0],
+              [9 , 0, 0, 0],
+              [0 , 8, 0, 0],
+              [0 , 6, 0, 0],
+              [0 , 7, 0, 0],
+              [0 , 0, 9, 0],
+              [0 , 0, 8, 0],
+              [0 , 0,10, 0],])
+k=3
 ks = init_kmeans(data, k)
 
 while True:
-    us = cluster_mean(data, ks)
     newks = update_cluster(data, ks)
 
     if newks == ks:
         break
     else:
         ks = newks
+
+print ks
