ece3e5c68ca4eb8ea1017aa4b2962c01ed032221
Casey Grun <casey.grun@gmail.com>
2014-02-07 00:23:38 -0500

    Refactored warmup to condense utility functions into kmeans.py. Implemented kmeans for sparse matrices using scipy.sparse

--- casey/warmup.py
+++ casey/warmup.py
@@ -9,38 +9,44 @@
 import numpy as np
 import kmeans
 
-def dist(x,u):
-	"""
-	Given an (n x d) array X, a (k x d) array U, returns a (k x n) array D 
-	giving the cartesian distances between each row of X and each row of U. 
-	That is, D[i,j] = distance(U[i,:], X[j,:])
-	"""
-	# https://github.com/dwf/rescued-scipy-wiki/blob/master/EricsBroadcastingDoc.rst
-	diff = x[np.newaxis,:,:] - u[:,np.newaxis,:]
-	dist = np.sum(diff**2,axis=-1)
-	return dist
+# load one part of the CIFAR-10 dataset
+d = kmeans.unpickle("../data/warmup/cifar-10-batches-py/data_batch_1")
+X = kmeans.standardize(d["data"])
 
-def roll(U,R):
-	"""
-	Rotates a given solution
-	"""
-	U = np.roll(U,1,axis=0)
-	R = np.roll(R,1,axis=1)
-	return (U,R)
-
-
-# Try a simple example that's easily verified
-X = np.array([[10, 0, 0, 0],
-			  [11, 0, 0, 0],
-			  [9 , 0, 0, 0],
-			  [0 , 8, 0, 0],
-			  [0 , 6, 0, 0],
-			  [0 , 7, 0, 0],
-			  [0 , 0, 9, 0],
-			  [0 , 0, 8, 0],
-			  [0 , 0,10, 0],])
-(U, R) = kmeans.cluster(X,3,dist)
-
+(U,R) = kmeans.cluster(X,10,kmeans.dist)
 print U
 print R
+
+kmeans.pickle(U,"U")
+kmeans.pickle(R,"R")
+
+np.savetxt("U.csv", U)
+np.savetxt("R.csv", R)
+
+
+
+
+# # Try a simple example that's easily verified
+# X = np.array([[10, 0, 0, 0],
+# 			  [11, 0, 0, 0],
+# 			  [9 , 0, 0, 0],
+# 			  [0 , 8, 0, 0],
+# 			  [0 , 6, 0, 0],
+# 			  [0 , 7, 0, 0],
+# 			  [0 , 0, 9, 0],
+# 			  [0 , 0, 8, 0],
+# 			  [0 , 0,10, 0],])
+# X = np.array([[10, 0, 0, 0],
+# 			  [11, 0, 0, 0],
+# 			  [9 , 0, 0, 0],
+# 			  [0 , 8, 0, 0],
+# 			  [0 , 6, 0, 0],
+# 			  [0 , 7, 0, 0],
+# 			  [0 , 0, 9, 0],
+# 			  [0 , 0, 8, 0],
+#			  [0 , 0,10, 0],])
+# (U, R) = kmeans.cluster(X,3,dist)
+
+# print U
+# print R
 
