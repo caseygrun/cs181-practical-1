5c3701e379431dca081173ba8b04ee67978df916
rhedshi <rhedshi@Rheds-MacBook-Pro.local>
2014-02-12 15:49:50 -0500

    created my own mfac folder

---
+++ rhed/mfac/books.py
@@ -0,0 +1,357 @@
+# ----------------------------------------------------------------------------
+# CS 181 | Practical 1 | Predictions
+# Casey Grun
+#
+# books.py
+# Contains shared utility functions for the books problem
+#
+# ----------------------------------------------------------------------------
+
+
+import sys
+import math
+import random
+import numpy as np
+import scipy.sparse as sp
+import shared_utils as su
+import util
+
+DEBUG = True
+def debug(fmt, arg=tuple()):
+	if DEBUG: print fmt % arg
+
+def rmse_withheld(trainData, ratings_data, mfact_data, use_biases=False):
+	"""
+	Takes a factorization produced from a set of training data, and calculates 
+	the RMSE with a set of withheld data.
+
+	Arguments:
+		train_data   	: data object from the training data
+		withheld_data	: data object from the withheld data
+		mfact_data   	: data object from mfact
+
+	Returns:
+		error			: RMSE between predicted ratings and actual ratings
+						from the withheld data
+	"""
+	# load data from the original training set
+	center = trainData["center"]
+	scale = trainData["scale"]
+	book_isbn_to_index = trainData["book_isbn_to_index"]
+
+	# load data calculated by the matrix factorization
+	P = mfact_data["P"]
+	Q = mfact_data["Q"]
+	Bn = mfact_data["Bn"]
+	Bd = mfact_data["Bd"]
+	mean = mfact_data["mean"]
+
+	error = 0.0
+
+	for (i,j,r) in ratings_data['ratings']:
+		
+		if(not use_biases):
+			predictedR = (np.dot(P[i,:],Q[j,:]) + mean) \
+				* scale + center
+		else:
+			predictedR = (np.dot(P[i,:],Q[j,:]) + mean + Bn[i] + Bd[j]) \
+				* scale + center
+		
+		#predictedR = np.dot(P[i,:],Q[j,:]) * scale + center
+		error += (r - predictedR)**2
+		# print r, predictedR
+
+	error /= len(ratings_data['ratings'])
+	error = math.sqrt(error)
+
+	return error
+
+
+def make_predictions(ratings_data, mfact_data):
+	"""
+	Makes a set of predictions, suitable for passing to util.write_predictions
+
+	Arguments:
+		ratings_data	: the data object returned by build_ratings
+		mfact_data 		: the data object returned by mfact
+
+	Returns:
+		predictions 	: a list of dicts, suitable for passing to util.write_predictions
+
+	"""
+
+	# load data from the original training set
+	center = ratings_data["center"]
+	scale = ratings_data["scale"]
+	book_isbn_to_index = ratings_data["book_isbn_to_index"]
+
+	# load data calculated by the matrix factorization
+	P = mfact_data["P"]
+	Q = mfact_data["Q"]
+	Bn = mfact_data["Bn"]
+	Bd = mfact_data["Bd"]
+	mean = mfact_data["mean"]
+
+	# load the set of requested predictions
+	queries = util.load_test("../data/books/ratings-test.csv")
+	L = len(queries)
+	debug("Making %d predictions",L)
+
+	# for each query
+	for (i,query) in enumerate(queries):
+
+		# print progress
+		# if DEBUG: print ("%d / %d : " % (i+1,L)),
+
+		# lookup user and book index
+		user_index = query["user"] - 1
+		book_index = book_isbn_to_index[query["isbn"]]
+
+		# calculate predicted rating
+		rating_float = (np.dot(P[user_index,:],Q[book_index,:]) + mean + Bn[user_index] + Bd[book_index]) \
+			* scale + center
+
+		# coerce to range (1,5); round
+		rating = max(1,min(5,rating_float))
+
+		# store both values so we can do visualization of distributions later
+		query["rating"] = rating
+		query["rating_f"] = rating_float
+
+		# print value
+		# if DEBUG: print "%f -> %d" % (rating_float, rating)
+
+	return queries
+
+def user_mean(data):
+	ratings = data['ratings']
+
+	N = data['N']
+	userTotal = np.zeros(N)
+	userCount = np.zeros(N)
+
+	for (i,j,r) in ratings:
+		userTotal[i] += r
+		userCount[i] += 1.0
+
+	return userTotal / userCount
+
+
+def build_ratings(filename="ratings_tuple_std", standardize=True, format="tuple", withhold=20000, discard=0):
+	"""
+	Loads the training data, standardizes it, withholds points, and converts it
+	to the desired format. This produces two dicts: `data_train` and
+	`data_withhold`. `data_train` is pickled and saved to `filename`, while
+	`data_withhold` is pickled and saved to `filename` + "_withheld"
+
+	Arguments:
+
+		filename	: File name to save the data to
+		standardize : one of:
+			-	True	: standardize the data by subtracting the mean and
+			dividing by the standard deviation
+			-	'linear': standardize by subtracting 1 and dividing by 4
+			-	False	: do not standardize
+		withhold : number of points to withhold (must be less than the number
+				of points in the training data)
+		discard : number of points to discard
+		format   	: one of:
+			-	"lil"	: encode the ratings as a scipy.sparse.lil_matrix
+			-	"tuple"	: encode the ratings as a list of tuples (i, j, r)
+			where i is the user index, j is the book index, and r is the rating
+
+	Returns: two dicts: data_train and data_withhold; each has the following
+	keys.
+
+		-	"ratings" : a list of tuples (i,j,r) where i is the user, j is the
+			book, and r is the rating that the user gave the book
+		-	"book_isbn_to_index" : dict that maps the ISBN for each book to a
+			numerical index j.
+		-	"N" : the number of users
+		-	"D" : the number of books
+		-	"T" : the number of ratings
+		-	"center" : the number used to offset the ratings (for standardization)
+		-	"scale" : the number used to scale the ratings (for standardization);
+			The ratings can be de-standardized by:
+			destandardized = standardized * scale + center
+		-	"mean" : the mean of the ratings
+		-	"variance" : the variance of the ratings
+	"""
+
+	# load ratings from CSV into list of tuples
+	data = build_ratings_tuple()
+
+	# partition ratings
+	(data_train, data_withhold) = partition_ratings(data,withhold,discard=discard)
+
+	# standardize ratings
+	print "Training data: "
+	data_train = standardize_ratings(data_train, standardize)
+
+	print "Withheld data: "
+	data_withhold = standardize_ratings(data_withhold, False)
+
+	# convert to LIL if desired
+	if(format=="lil"):
+		data_train["ratings"] = ratings_tuple_to_lil(data_train["ratings"], data_train["N"], data_train["D"])
+		data_withhold["ratings"] = ratings_tuple_to_lil(data_withhold["ratings"], data_withhold["N"], data_withhold["D"])
+
+	# save
+	print "Saving training data to %s" % filename
+	su.pickle(data_train,filename)
+
+	filename_withheld = filename+"_withheld"
+	print "Saving withheld data to %s" % filename_withheld
+	su.pickle(data_withhold,filename_withheld)
+
+def build_ratings_tuple():
+	"""
+	Loads the training data for N users and D books, and builds a list of tuples
+
+	Returns: a dict with the following fields:
+
+		-	`ratings` : a list of tuples (i,j,r) where i is the user, j is the
+			book, and r is the rating that the user gave the book
+		-	`book_isbn_to_index` : dict that maps the ISBN for each book to a
+			numerical index j.
+		-	`N` : the number of users
+		-	`D` : the number of books
+		-	`T` : the number of ratings in the training set
+	"""
+
+	print "Loading Users..."
+	users = util.load_users("../data/books/users.csv")
+	user_ids = sorted([ user["user"] for user in users ])
+	N = len(user_ids)
+	del users
+	print "Loaded %d users." % N
+
+
+	print "Loading Books..."
+	books = util.load_books("../data/books/books.csv")
+	book_isbns = sorted([ book["isbn"] for book in books ])
+	book_isbn_to_index = dict( zip(book_isbns,range(len(book_isbns))) )
+	D = len(book_isbns)
+	print "Loaded %d books." % D
+
+
+	print "Loading Trainings..."
+	train = util.load_train("../data/books/ratings-train.csv")
+	T = len(train)
+	print "Loaded %d ratings." % T
+	ratings = [(rating["user"]-1, book_isbn_to_index[rating["isbn"]], (rating["rating"])) for rating in train]
+
+	return { "ratings": ratings, "book_isbn_to_index": book_isbn_to_index , \
+		"N": N, "D": D, "T": T}
+
+def partition_ratings(data,withhold=0,discard=0):
+	"""
+	Partitions a list of rankings randomly into a set to be trained and a set
+	to be withheld.
+
+	Arguments:
+		data 		: data object of the form returned by build_ratings_tuple
+		withhold	: number of ratings to withhold (must be < len(ratings))
+		discard		: number of ratings to discard (must be < len(ratings) - withhold)
+
+	Returns:
+		data_train	: data object to train on
+		data_withhold : data object to withhold
+
+	"""
+
+	# create clone of data for withholding
+	data_train = data
+	data_withhold = data.copy()
+	ratings = data["ratings"]
+
+	print "Witholding %d ratings, training on %d, discarding %d" % (withhold, len(ratings)-withhold-discard, discard)
+
+	# randomly permute ratings
+	random.shuffle(ratings)
+
+	# partition ratings into training set and withheld set
+	data_withhold["ratings"] = ratings[0:withhold]
+	data_withhold["T"] = len(data_withhold["ratings"])
+	assert len(data_withhold["ratings"]) == withhold
+
+	data_train["ratings"] = ratings[withhold:len(ratings)-discard]
+	data_train["T"] = len(data_train["ratings"])
+	assert len(data_train["ratings"]) == (len(ratings)-withhold-discard)
+
+	return (data_train, data_withhold)
+
+def standardize_ratings(data, mode=True):
+	"""
+	Standardizes a set of ratings
+
+	Arguments:
+
+		data	: dict of the form returned by build_ratings_tuple
+		mode	: one of:
+			-	True	: standardize the data by subtracting the mean and
+			dividing by the standard deviation
+			-	'linear': standardize by subtracting 1 and dividing by 4
+			-	False	: do not standardize
+
+	Returns:
+
+		data	: dict of the form returned by build_ratings, but augmented
+			with the following keys
+			-	"center"	: value subtracted from each rating
+			-	"scale"		: rating is divided by the sqrt of this value
+			-	"mean"		: mean of the training data
+			-	"variance"	: variance of the training data
+
+	"""
+
+	print "Calculating mean and variance..."
+	T = len(data["ratings"])
+	x = 0.0
+	x2 = 0.0
+
+	# calculate mean and variance
+	for (i,j,Rij) in data["ratings"]:
+		x += Rij
+		x2 += Rij**2
+
+	mean = x / T
+	var = (x2/T) - mean**2
+	std = math.sqrt(var)
+	print "Mean: %f , Variance: %f , Std. Dev: %f" % (mean, var, std)
+
+	if(mode == True):
+
+		# build up the sum of the values and the squared values of each rating, in order to calculate the mean and variance
+		print "Standardizing ratings..."
+		center = mean
+		scale = std
+
+	elif (mode == 'linear'):
+
+		print "Standardizing ratings to the interval [0,1]..."
+		center = 1.0
+		scale = 4.0
+
+	else:
+
+		print "No standardization applied to ratings."
+		center = 0.0
+		scale = 1.0
+
+	data["ratings"] = [ (i, j, (Rij - center)/scale) for (i, j, Rij) in data["ratings"]]
+	data["center"] = center
+	data["scale"] = scale
+	data["mean"] = mean
+	data["variance"] = var
+	return data
+
+def ratings_tuple_to_lil(ratings_tuple, N, D):
+	"""
+	Converts a list of tuples to an N x D
+	scipy.sparse.lil_matrix
+	"""
+	ratings = sp.lil_matrix((N,D))
+	for (i, j, rating) in ratings_tuple:
+		ratings[i,j] = rating
+	return ratings
---
+++ rhed/mfac/check_grad.py
@@ -0,0 +1,164 @@
+# ----------------------------------------------------------------------------
+# CS 181 | Practical 1 | Predictions
+# Casey Grun
+# 
+# check_grad.py
+# Tries to check the gradient with a finite-difference approximation
+# ----------------------------------------------------------------------------
+
+import numpy as np
+import numpy.linalg as la
+import shared_utils as su
+import random
+import math
+import scipy.optimize
+
+def check(N, D, K, alpha=0.01, beta=0.02, epsilon=1e-6):
+
+	def unroll(A):
+		"""
+		Takes a big matrix A and splits it up into P, Q, Bn, and Bd
+		"""
+		P  = A[0             : N*K ]             .copy().reshape(N,K)
+		Q  = A[N*K           : N*K + D*K ]       .copy().reshape(D,K)
+		Bn = A[N*K + D*K     : N*K + D*K + N ]   .copy().reshape(N,1)
+		Bd = A[N*K + D*K + N : N*K + D*K + N + D].copy().reshape(D,1)
+		return (P,Q,Bn,Bd)
+
+	def compact(P,Q,Bn,Bd):
+		"""
+		Takes four matrices (P, Q, Bn, and Bd) and concatenates them into one
+		flat matrix A
+		"""
+		return np.concatenate((P.flat, Q.flat, Bn.flat, Bd.flat)).copy()
+
+	def error(A,R):
+		"""
+		Calculates the errors
+		"""
+		(P,Q,Bn,Bd) = unroll(A.copy())
+
+		# calculate the total error, e
+		e = 0
+		for (i,j,Rij) in R:
+			eij = Rij - (mean + Bn[i] + Bd[j] + np.dot(P[i,:],Q[j,:]))
+			e += eij**2
+
+			# e += (beta/2) * (Bn[i]**2 + Bd[j]**2)
+			# e += (beta/2) * (np.sum(P[i,:]**2) + np.sum(Q[j,:]**2))
+
+		# e = e + (beta/2) * (sum(Bn**2) + sum(Bd**2) + la.norm(P) + la.norm(Q))
+		e = e + (beta/2) * (sum(Bn**2) + sum(Bd**2) + np.sum(P**2) + np.sum(Q**2))
+
+		return e
+
+	def grad(A,R):
+		(P,Q,Bn,Bd) = unroll(A.copy())
+
+		dP = np.zeros_like(P)
+		dQ = np.zeros_like(Q)
+		dBn = np.zeros_like(Bn)
+		dBd = np.zeros_like(Bd)
+
+		# calculate gradient
+		for (i,j,Rij) in R:
+			eij = Rij - (mean + Bn[i] + Bd[j] + np.dot(P[i,:],Q[j,:]))
+			dP[i,:] += -1. * (2 * eij * Q[j,:] - beta * P[i,:])
+			dQ[j,:] += -1. * (2 * eij * P[i,:] - beta * Q[j,:])
+			dBn[i]  += -1. * (2 * eij          - beta * Bn[i]) 
+			dBd[j]  += -1. * (2 * eij          - beta * Bd[j]) 
+			# dBn[i] = 0.
+			# dBd[j] = 0.
+
+
+		# return gradient
+		return compact(dP, dQ, dBn, dBd)
+
+	# scipy's check_grad function only lets us use a flat, 1-D array, so I 
+	# concatenate all our input coordinates together into one long flat 1-D 
+	# array.
+	
+	# pick a random place in coordinate space to start out
+	A = np.random.random_sample((N*K + D*K + N +D,))
+	# A[N*K + D*K:] = 0
+	assert (A == compact(*unroll(A))).all()
+
+	# generate some random data for R
+	samples = max(int(math.ceil(N*D/2)),1)
+	chosen = set()
+	R = []
+	for s in xrange(samples):
+		i = random.randrange(N)
+		j = random.randrange(D)
+		Rij = random.uniform(-1,1)
+		if (i,j) not in chosen:
+			R += [(i, j, Rij)]	
+			chosen.add((i,j))
+
+	# # uncomment this to set values for all i,j in R
+	# R = [(i,j,random.uniform(0,1)) for i in xrange(N) for j in xrange(D)]
+	
+	# calculate mean of values in R
+	mean = 1./len(R) * float(sum([ Rij for (i,j,Rij) in R ]))
+
+	def check_grad(func,grad,x0,*args):
+		g = grad(x0, *args)
+		f = scipy.optimize.approx_fprime(x0, func, epsilon, *args)
+		err = np.sqrt(sum((g - f)**2)/len(x0))
+		print "G: ", 
+		print g
+		print "F: ",
+		print f
+		print "G - F: ",
+		print g - f
+		print "RMSE: ",
+		print err
+		return err
+
+	return check_grad(error,grad,A,R)
+	# return scipy.optimize.check_grad(error,grad,A,R)
+
+
+# print check(50,50,1)
+
+# check 10 times and average
+# enter different values for N, D, and K here. 
+rmses = [check(50,50,50, beta=0.02) for x in xrange(10)]
+print "Average error: ",
+print np.mean(rmses)
+sys.exit(0)
+
+# ------------------------------------------------------------
+
+# check different values for N, D, K
+values = [1, 5, 10, 25, 50]
+rmses = np.zeros((len(values),len(values),len(values)))
+for (i,N) in enumerate(values):
+	for (j,D) in enumerate(values):
+		for (k,K) in enumerate(values):
+			rmses[i,j,k] = check(N,D,K, beta=0.02)
+			print "N = %d \t D = %d \t K = %d \t RMSE = %d" % (N,D,K,rmses[i,j,k])
+
+su.pickle((values,rmses),"check_grad_data_b_0.02")
+sys.exit(0)
+
+# ------------------------------------------------------------
+
+# plot gradient RMSE
+(values,rmses) = su.unpickle("check_grad_data")
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+for (i,N) in enumerate(values):
+	for (j,D) in enumerate(values):
+		print (i,j), (N,D)
+		plt.subplot(len(values),len(values),j*len(values)+i+1)
+		plt.plot(values,rmses[i,j,:])
+		plt.title("N = %d, D = %d" % (N, D))
+		plt.xlabel("K")
+		plt.ylabel("RMSE")
+		plt.ylim(0,100)
+
+plt.show()
+
