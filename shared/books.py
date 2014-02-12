Beta = 0, fixed gradient descent

shared/books.py
@@ -38,11 +38,11 @@ def rmse_withheld(trainData, ratings_data, mfact_data):
 	for (i,j,r) in ratings_data['ratings']:
 		#predictedR = (np.dot(P[i,:],Q[j,:]) + mean + Bn[i] + Bd[j]) \
 		#	* scale + center
-		predictedR = (np.dot(P[i,:],Q[j,:]) + mean) \
-			* scale + center
+		predictedR = np.dot(P[i,:],Q[j,:]) * scale + center
 		#predictedR = np.dot(P[i,:],Q[j,:]) * scale + center
+		predictedR = max(1,min(5,predictedR))
 		error += (r - predictedR)**2
-		print P[i,:],Q[j,:],predictedR, r
+		#print P[i,:],Q[j,:],predictedR,r
 
 	error /= len(ratings_data['ratings'])
 	error = math.sqrt(error)
@@ -91,11 +91,12 @@ def make_predictions(ratings_data, mfact_data):
 		book_index = book_isbn_to_index[query["isbn"]]
 
 		# calculate predicted rating
-		rating_float = (np.dot(P[user_index,:],Q[book_index,:]) + mean + Bn[user_index] + Bd[book_index]) \
-			* scale + center
+		#rating_float = (np.dot(P[user_index,:],Q[book_index,:]) + mean + Bn[user_index] + Bd[book_index]) \
+		#	* scale + center
+		rating_float = np.dot(P[user_index,:],Q[book_index,:]) * scale + center
 
 		# coerce to range (1,5); round
-		rating = max(1,min(5,rating_float[0]))
+		rating = max(1,min(5,rating_float))
 
 		# store both values so we can do visualization of distributions later
 		query["rating"] = rating

shared/mf_debug.py
@@ -20,7 +20,7 @@ def debug(fmt, arg=tuple()):
     if DEBUG: print fmt % arg
 
 # steps=5000, alpha=0.0002, beta=0.02, epsilon=0.001, save_every=20
-def mfact(R, N, D, K, steps=500, alpha=0.01, beta=0.02, epsilon=0.1, save_every=20, filename=None):
+def mfact(R, N, D, K, steps=500, alpha=0.01, beta=0.0, epsilon=0.1, save_every=20, filename=None):
     """
     Adapted from Albert Au Yeung (2010)
     http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/
@@ -46,8 +46,8 @@ def mfact(R, N, D, K, steps=500, alpha=0.01, beta=0.02, epsilon=0.1, save_every=
     """
 
     # initialize random user and book feature matrices
-    P = np.random.rand(N,K)
-    Q = np.random.rand(D,K)
+    P = np.random.rand(N,K) * 4 + 1
+    Q = np.random.rand(D,K) * 4 + 1
     #Q = np.ones((D,K))
 
     # initialize random bias vectors
@@ -83,7 +83,8 @@ def mfact(R, N, D, K, steps=500, alpha=0.01, beta=0.02, epsilon=0.1, save_every=
         e = 0
         for (i,j,Rij) in R:
             #eij = Rij - (mean + Bn[i] + Bd[j] + np.dot(P[i,:],Q[j,:]))
-            eij = Rij - (mean + np.dot(P[i,:],Q[j,:]))
+            #eij = Rij - (mean + np.dot(P[i,:],Q[j,:]))
+            eij = Rij - np.dot(P[i,:],Q[j,:])
             e += pow(eij, 2)
 
         #e = e + (beta/2) * (sum(Bn**2) + sum(Bd**2) + la.norm(P) + la.norm(Q))
@@ -112,9 +113,10 @@ def mfact(R, N, D, K, steps=500, alpha=0.01, beta=0.02, epsilon=0.1, save_every=
             Q[j,:] = Q[j,:] + alpha * (2 * eij * P[i,:] - beta * Q[j,:])
             Bn[i]  = Bn[i]  + alpha * (2 * eij          - beta * Bn[i])
             Bd[j]  = Bd[j]  + alpha * (2 * eij          - beta * Bd[j])"""
-            eij = Rij - (mean + np.dot(P[i,:],Q[j,:]))
+            eij = Rij - np.dot(P[i,:],Q[j,:])
+            PiTemp = P[i,:]
             P[i,:] = P[i,:] + alpha * (2 * eij * Q[j,:] - beta * P[i,:])
-            Q[j,:] = Q[j,:] + alpha * (2 * eij * P[i,:] - beta * Q[j,:])
+            Q[j,:] = Q[j,:] + alpha * (2 * eij * PiTemp - beta * Q[j,:])
 
     if step == steps-1:
         print "Gave up after %d steps with delta-error = %d" % (step+1, de)
@@ -179,7 +181,7 @@ def mfact_cont(R, N, D, K, cont_file, steps=500, alpha=0.01, beta=0.02, epsilon=
         e = 0
         for (i,j,Rij) in R:
             #eij = Rij - (mean + Bn[i] + Bd[j] + np.dot(P[i,:],Q[j,:]))
-            eij = Rij - (mean + np.dot(P[i,:],Q[j,:]))
+            eij = Rij - np.dot(P[i,:],Q[j,:])
             e += pow(eij, 2)
 
         #e = e + (beta/2) * (sum(Bn**2) + sum(Bd**2) + la.norm(P) + la.norm(Q))
@@ -208,7 +210,7 @@ def mfact_cont(R, N, D, K, cont_file, steps=500, alpha=0.01, beta=0.02, epsilon=
             Q[j,:] = Q[j,:] + alpha * (2 * eij * P[i,:] - beta * Q[j,:])
             Bn[i]  = Bn[i]  + alpha * (2 * eij          - beta * Bn[i])
             Bd[j]  = Bd[j]  + alpha * (2 * eij          - beta * Bd[j])"""
-            eij = Rij - (mean + np.dot(P[i,:],Q[j,:]))
+            eij = Rij - np.dot(P[i,:],Q[j,:])
             P[i,:] = P[i,:] + alpha * (2 * eij * Q[j,:] - beta * P[i,:])
             Q[j,:] = Q[j,:] + alpha * (2 * eij * P[i,:] - beta * Q[j,:])
 